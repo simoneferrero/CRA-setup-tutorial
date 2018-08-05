daaa14e525ce8cf50af9053bdf4de7784366dd09
Simone Ferrero <simone.ferrero@sohonet.com>
2018-08-05 19:27:46 +0100

    step-2.5-refinement - Modified reducer to use state for active logo

--- src/reducers/logoReducer/index.js
+++ src/reducers/logoReducer/index.js
@@ -5,8 +5,10 @@ import initialState from './initialState'
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case CHANGE_SPEED:
-      return state.setIn(['logos', 'reactLogo', 'speed'], action.speed)
+    case CHANGE_SPEED: {
+      const activeLogo = state.get('activeLogo')
+      return state.setIn(['logos', activeLogo, 'speed'], action.speed)
+    }
 
     case SET_ACTIVE_LOGO: {
       return state.set('activeLogo', action.logo)
